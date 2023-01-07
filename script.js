dec2a7aebb95fcdf140db3a46627ee33314f1e4e
Hello747 <122001198+Hello747@users.noreply.github.com>
2023-01-07 12:30:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+(function($){
+	$(function(){
+		
+		$('.navbar ul li a:not(:only-child)').click(function(e){
+			$(this).siblings('.nav-dropdown').toggle();
+			$('.nav-dropdown').not($(this).siblings()).hide();
+			e.stopPropagation();
+});
+
+	$('html').click(function(){
+		$('.nav-dropdown').hide();
+	});
+
+	});
+})(jQuery);
